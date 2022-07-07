7df760eb276c1e60375cbac696860874e2b62520
Parth <parthshreya@hotmail.com>
2022-07-07 18:21:38 -0400

    hi

--- Card.py
+++ Card.py
@@ -1,6 +1,6 @@
 
 import random
-import string
+
 from tkinter.messagebox import QUESTION
 
 class cardclass():
@@ -14,41 +14,39 @@ class cardclass():
         self.inputdata = data
         self.data = ""
         self.FormatSorter()
-
     def FormatSorter(self):
         if(self.formatType == 0): self.blanksformat()
         if(self.formatType == 1): self.longanswerformat()
         if(self.formatType == 2): self.multichoiceformat()
         if(self.formatType == 3): self.truefalseformat()
         if(self.formatType == 4): self.standardformat()
-    def __formatinput__(self):
+    def formatinput(self):
         rawdata = self.inputdata
         question = rawdata[0]
         answer = rawdata[1]
         return question,answer
     def blanksformat(self):
         q,a = self.formatinput()
+        
         self.data = [q,a]
-        q2= string(q)
+        q2= str(q)
         for items in a:
-            q2.replace("-", string(items), 1)
+           q2 =  q2.replace("-", items, 1)
         self.data = [q,q2]
-        print(self.data)
     def longanswerformat(self):
         q,a = self.formatinput()
-        q = string(q)
-        a = string(a)
+        q = str(q)
+        a = str(a)
         self.data = [q,a.lower()]
     def multichoiceformat(self):
         q = self.inputdata
         a = q[len(q)-1]
         q.pop(len(q)-1)
-        self.data = [q,a]
-        
+        self.data = [q,a]        
     def truefalseformat(self):
         q,a = self.formatinput()
-        q = string(q)
-        a = string(a) 
+        q = str(q)
+        a = str(a) 
         a=a.lower() 
         if(a[0] == 't'):
             self.data = [q,True]
@@ -56,8 +54,8 @@ class cardclass():
             self.data = [q,False]
     def standardformat(self):
         q,a = self.formatinput()
-        q = string(q)
-        a = string(a)
+        q = str(q)
+        a = str(a)
         self.data = [q,a] 
 
         
@@ -68,4 +66,3 @@ class cardclass():
 
         
 
-thing = cardclass(1,1,1,1,1,2,[,[]],3)
